89cb50082b7a8195a7cb5ade0455391c6a25b55b
psy2r <79797760+psy2r@users.noreply.github.com>
2021-02-28 15:12:47 +0000

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,22 @@
+
+
+def menu():
+    error = False
+    start = True
+    while start == True:
+        while error == False:
+            try:
+                print("Would you like to Encrypt or Decrypt? (1 or 2)")
+                print("1. Encrypt")                    
+                print("2. Decrypt")
+                print()
+                choice = int(input("Choice:"))
+                if choice == 1:
+                    error = False
+                elif choice == 2:
+                    error = False
+            except:
+                error = True
+                print("Please try again")
+                menu()
+menu()
