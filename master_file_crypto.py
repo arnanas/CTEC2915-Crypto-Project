ebf79151bee6fa636c666e104574ad42f7d1abfb
psy2r <79797760+psy2r@users.noreply.github.com>
2021-03-11 00:42:33 +0000

    Update master_file_crypto.py

--- master_file_crypto.py
+++ master_file_crypto.py
@@ -1,11 +1,12 @@
 #This is the master file for everything to be chucked in together
 
+#Creating a procedure to hold the main menu procedure
 def menu():
-    error = False
-    start = True
+    error = False  #creating  two empty boolean values for while loops
+    start = True #those while loops are used for robustness
     while start == True:
         while error == False:
-            try:
+            try: #different options users can select from
                 print("Would you like to Encrypt or Decrypt? (1 or 2)")
                 print("1. Encrypt")                    
                 print("2. Decrypt")
@@ -18,8 +19,8 @@ def menu():
                     error = False
                 elif choice == 3:
                     quit()
-            except:
-                error = True
+            except: #if coice isnt one of the options 
+                error = True #loop whacks the user to the beggining
                 print("Please try again")
                 menu()
 menu()
